beaa5ca827a130a9de75077fa3a686cca834b73c
indra purnomo Aji <79568712+innoji26@users.noreply.github.com>
2023-05-16 19:53:40 +0700

    Update dbConfig.js

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports = {
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  pass: process.env.MYSQLPASSWORD,
-  db: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  pass: process.env.PASSWORD,
+  db: process.env.DATABASE,
+  port: process.env.PORT,
   dialect: 'mysql',
   pool: {
     max: 5,
